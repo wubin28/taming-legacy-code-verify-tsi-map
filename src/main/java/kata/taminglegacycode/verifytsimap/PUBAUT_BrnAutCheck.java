8751f152f80257bebac12f4433e8accd7029649b
benwu <wubinben@icloud.com>
2019-08-15 17:14:39 +0800

    make PUBAUT_BrnAutCheck.checkTsiTyp() unsupported

--- src/main/java/kata/taminglegacycode/verifytsimap/PUBAUT_BrnAutCheck.java
+++ src/main/java/kata/taminglegacycode/verifytsimap/PUBAUT_BrnAutCheck.java
@@ -2,6 +2,6 @@ package kata.taminglegacycode.verifytsimap;
 
 public class PUBAUT_BrnAutCheck {
     public static void checkTsiTyp(WorkflowServiceImpl workflowService, String pUsrOam, String xStrBbk, String xTsiTyp) {
-
+        throw new UnsupportedOperationException("PUBAUT_BrnAutCheck.checkTsiTyp() not supported yet.");
     }
 }
