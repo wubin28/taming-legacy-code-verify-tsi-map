f02c3835f7c8cf39004c1a0bc208e8e051dbb03d
benwu <wubinben@icloud.com>
2019-08-15 16:56:40 +0800

    update 6 test names

--- src/test/java/kata/taminglegacycode/verifytsimap/WorkflowServiceImplTest.java
+++ src/test/java/kata/taminglegacycode/verifytsimap/WorkflowServiceImplTest.java
@@ -6,7 +6,14 @@ import org.junit.Test;
 
 public class WorkflowServiceImplTest {
     @Test
-    public void shouldAnswerWithTrue() {
+    public void wTsiMap_should_not_be_null() {
         assertTrue(true);
     }
+
+    //xFliSts_in_wTsiMap_should_not_be_null
+    //xFliSts_in_wTsiMap_should_be_01
+    //xTsiSts_in_wTsiMap_should_be_01
+    //xTskCod_in_wTsiMap_should_be_in_wBasTypLst
+    //xOprUsr_in_wTsiMap_should_be_equal_to_pUsrOam
+
 }
